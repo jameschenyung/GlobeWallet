5b76d1594a37fa10aa3c35e5487b8386b0cbb1d7
yoyogong <yoyogongtian@gmail.com>
2023-10-31 20:22:05 -0400

    SignInPage UI update

--- src/views/SignInPage.java
+++ src/views/SignInPage.java
@@ -1,3 +1,5 @@
+package views;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,6 +10,7 @@ public class SignInPage implements ActionListener {
     private static JTextField userText;
     private static JLabel success;
     private static JButton logInButton;
+    private static JLabel logInLabel;
     private static JPasswordField passwordText;
 
     public static void main(String[] args) {
@@ -20,29 +23,33 @@ public class SignInPage implements ActionListener {
 
         SignInPanel.setLayout(null);
 
-        userLabel = new JLabel("User Name");
-        userLabel.setBounds(10, 20, 100, 25);
+        logInLabel = new JLabel("LOGIN");
+        logInLabel.setBounds(10, 20, 100, 25);
+        SignInPanel.add(logInLabel);
+
+        userLabel = new JLabel("Username");
+        userLabel.setBounds(10, 50, 100, 25);
         SignInPanel.add(userLabel);
 
         userText = new JTextField(20);
-        userText.setBounds(100, 20, 200, 25);
+        userText.setBounds(10, 70, 200, 25);
         SignInPanel.add(userText);
 
         passwordLabel = new JLabel("Password");
-        passwordLabel.setBounds(10, 50, 100, 25);
+        passwordLabel.setBounds(10, 100, 100, 25);
         SignInPanel.add(passwordLabel);
 
         passwordText = new JPasswordField();
-        passwordText.setBounds(100, 50, 200, 25);
+        passwordText.setBounds(10, 120, 200, 25);
         SignInPanel.add(passwordText);
 
         logInButton = new JButton("Login");
-        logInButton.setBounds(10, 80, 80, 25);
+        logInButton.setBounds(10, 150, 80, 25);
         logInButton.addActionListener(new SignInPage());
         SignInPanel.add(logInButton);
 
         success = new JLabel("");
-        success.setBounds(10, 110, 300, 25);
+        success.setBounds(10, 180, 300, 25);
         SignInPanel.add(success);
 
         frame.setVisible(true);
@@ -53,7 +60,7 @@ public class SignInPage implements ActionListener {
         String user = userText.getText();
         String password = passwordText.getText();
 
-        if (user.equals("test") && (password.equals("test"))) {
+        if (user.equals("test") && (password.equals("test1234"))) {
             success.setText("Login successful!");
         }
     }
