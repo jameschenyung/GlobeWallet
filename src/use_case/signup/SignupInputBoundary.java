bfe203fd095cfc1fa4ef76d1428467558539214f
keiserj <keiser.jiang@gmail.com>
2023-11-16 15:52:40 -0500

    set up interface. DataAccessObject.java implements LoginUserDataAccessInterface.getUserByUsername & validatePassword

--- src/use_case/signup/SignupInputBoundary.java
+++ src/use_case/signup/SignupInputBoundary.java
@@ -1,4 +1,4 @@
 package use_case.signup;
 
-public class SignupInputBoundary {
+public interface SignupInputBoundary {
 }
--- src/use_case/signup/SignupOutputBoundary.java
+++ src/use_case/signup/SignupOutputBoundary.java
@@ -1,4 +1,4 @@
 package use_case.signup;
 
-public class SignupOutputBoundary {
+public interface SignupOutputBoundary {
 }
