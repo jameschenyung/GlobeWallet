89b674db40b856d275124ac51d56b090b67e6ef2
keiserj <keiser.jiang@gmail.com>
2023-11-16 16:43:02 -0500

    Added implementation for SignupUserDataAccessInterface

--- src/database/DataAccessObject.java
+++ src/database/DataAccessObject.java
@@ -8,25 +8,13 @@ import java.sql.*;
 import objects.User;
 import objects.Account;
 
-public class DataAccessObject implements use_case.login.LoginUserDataAccessInterface{
+public class DataAccessObject implements use_case.login.LoginUserDataAccessInterface,
+        use_case.signup.SignupUserDataAccessInterface{
     private static final String DB_URL = "jdbc:sqlite:bank.db";
 
     // Establish database connection
     private Connection connect() throws SQLException {
         return DriverManager.getConnection(DB_URL);
-    }
-
-    // Save user data
-    public void saveUser(String firstName, String lastName, String username, String password) throws SQLException {
-        String sql = "INSERT INTO users (firstName, lastName, username, password) VALUES (?, ?, ?, ?)";
-        try (Connection conn = this.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, firstName);
-            pstmt.setString(2, lastName);
-            pstmt.setString(3, username);
-            pstmt.setString(4, password);
-            pstmt.executeUpdate();
-        }
     }
 
     // Save account data
@@ -141,5 +129,59 @@ public class DataAccessObject implements use_case.login.LoginUserDataAccessInter
             e.printStackTrace();
         }
         return false;
+    }
+
+    @Override
+    public User createUser(String firstName, String lastName, String username, String password) {
+        String sql = "INSERT INTO users (firstName, lastName, username, password) VALUES (?, ?, ?, ?)";
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.setString(3, username);
+            pstmt.setString(4, password);
+
+            int affectedRows = pstmt.executeUpdate();
+
+            // Check the affected rows
+            if (affectedRows > 0) {
+                // Assuming User class has a constructor that matches this data
+                try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        return new User(generatedKeys.getInt(1), firstName, lastName, username, password);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameTaken(String username) {
+        String sql = "SELECT COUNT(id) FROM users WHERE username = ?";
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, username);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean validatePassword(String password) {
+        // Regex to check for a password that has at least one uppercase letter,
+        // at least one lowercase letter, at least one digit, no symbols,
+        // and is at least 8 characters long.
+        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$";
+
+        // Check if the password matches the regex
+        return password != null && password.matches(passwordRegex);
     }
 }
--- src/use_case/login/LoginInputBoundary.java
+++ src/use_case/login/LoginInputBoundary.java
@@ -1,4 +1,4 @@
 package use_case.login;
 
-public class LoginInputBoundary {
+public interface LoginInputBoundary {
 }
