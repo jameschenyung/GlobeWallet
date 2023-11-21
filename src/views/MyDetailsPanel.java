ac1681f1754f63db649125c7775b24ae46af80f8
yoyogong <yoyogongtian@gmail.com>
2023-11-20 20:57:45 -0500

    UI updates - updated UI design for RegisterPanel and SignInPanel - updated UI design for the home page - added some buttons for AccountPanel for further implementation

---
+++ src/views/MyDetailsPanel.java
@@ -0,0 +1,9 @@
+package views;
+
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+
+public class MyDetailsPanel extends JPanel{
+    public MyDetailsPanel(MainFrame frame){}
+}
--- src/views/SignInPanel.java
+++ src/views/SignInPanel.java
@@ -1,10 +1,12 @@
 package views;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SignInPanel extends JPanel implements ActionListener {
+    private JLabel welcomeLabel;
     private JLabel signinLabel;
     private JLabel userLabel;
     private JLabel passwordLabel;
@@ -19,28 +21,39 @@ public class SignInPanel extends JPanel implements ActionListener {
         this.frame = frame;
         setLayout(null);
 
-        signinLabel = new JLabel("LOG IN");
-        signinLabel.setBounds(10, 20, 100, 25);
+        welcomeLabel = new JLabel("WELCOME!");
+        welcomeLabel.setBounds(10, 20, 100, 25);
+        Font laFont = welcomeLabel.getFont();
+        float newWelcomeSize = 18.0f;
+        welcomeLabel.setFont(laFont.deriveFont(newWelcomeSize));
+        add(welcomeLabel);
+
+        signinLabel = new JLabel("Log in to continue. ");
+        signinLabel.setBounds(10, 40, 200, 25);
+        Font labelFont = signinLabel.getFont();
+        float newSize = 12.0f;
+        signinLabel.setFont(labelFont.deriveFont(newSize));
+        signinLabel.setForeground(Color.GRAY);
         add(signinLabel);
 
         userLabel = new JLabel("Username");
-        userLabel.setBounds(10, 50, 100, 25);
+        userLabel.setBounds(10, 70, 100, 25);
         add(userLabel);
 
         userText = new JTextField(20);
-        userText.setBounds(10, 70, 200, 25);
+        userText.setBounds(10, 90, 200, 25);
         add(userText);
 
         passwordLabel = new JLabel("Password");
-        passwordLabel.setBounds(10, 100, 100, 25);
+        passwordLabel.setBounds(10, 110, 100, 25);
         add(passwordLabel);
 
         passwordText = new JPasswordField();
-        passwordText.setBounds(10, 120, 200, 25);
+        passwordText.setBounds(10, 130, 200, 25);
         add(passwordText);
 
         logInButton = new JButton("Login");
-        logInButton.setBounds(10, 150, 80, 25);
+        logInButton.setBounds(10, 160, 80, 25);
         logInButton.addActionListener(this);
         add(logInButton);
 
