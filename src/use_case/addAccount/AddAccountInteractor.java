7d79316289557481656a37682b4b8123d6584c1a
jasminewang <haoying.wang@mail.utoronto.ca>
2023-11-30 18:30:35 -0500

    modified use case for addaccount

--- src/use_case/addAccount/AddAccountInteractor.java
+++ src/use_case/addAccount/AddAccountInteractor.java
@@ -2,10 +2,10 @@ package use_case.addAccount;
 
 import objects.Account;
 import objects.User;
-import database.DataAccessObject; // This should be the path to your DAO implementation
+import database.DataAccessObject;
 
 public class AddAccountInteractor implements AddAccountInputBoundary {
-    private DataAccessObject dataAccess; // Direct reference to DAO instead of through an interface
+    private DataAccessObject dataAccess;
     private AddAccountOutputBoundary outputBoundary;
 
     public AddAccountInteractor(DataAccessObject dataAccess, AddAccountOutputBoundary outputBoundary) {
@@ -16,18 +16,17 @@ public class AddAccountInteractor implements AddAccountInputBoundary {
     @Override
     public void addAccount(AddAccountInputData inputData) {
         try {
-            // Replace the findUserById method call with the direct DAO call
+
             User user = dataAccess.getUser(inputData.getUserId());
             if (user == null) {
                 outputBoundary.presentAddAccountResult(new AddAccountOutputData(false, "User not found"));
                 return;
             }
 
-            // Create a new Account with the given details. Assuming the Account constructor matches your provided code.
-            // Since Account constructor sets balance to 0.0, we don't need to pass it
+
             Account newAccount = new Account(null, user.getUserId(), 0.0, inputData.getCurrencyType());
 
-            // Add the new account to the user's list of accounts
+
             user.addAccount(newAccount);
             String accountId = String.valueOf(newAccount.getAccountId());
 
@@ -35,7 +34,6 @@ public class AddAccountInteractor implements AddAccountInputBoundary {
 
             dataAccess.saveAccount(accountId, userId, newAccount.getBalance());
 
-            // Notify the output boundary of the result
             outputBoundary.presentAddAccountResult(new AddAccountOutputData(true, "Account successfully added"));
         } catch (Exception e) {
             outputBoundary.presentAddAccountResult(new AddAccountOutputData(false, e.getMessage()));
