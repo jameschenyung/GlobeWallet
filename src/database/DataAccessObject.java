e2b353b2a0c474d86241889b227a77817c034d74
jameschenyung <jameschenyung@gmail.com>
2023-12-01 15:49:47 -0500

    method to get the email from database

--- src/database/DataAccessObject.java
+++ src/database/DataAccessObject.java
@@ -98,6 +98,22 @@ public class DataAccessObject implements use_case.login.LoginUserDataAccessInter
         return null; // Or throw an exception
     }
 
+    public String getEmail(int userId) {
+        String sql = "SELECT email FROM users WHERE id = ?";
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, userId);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rs.getString("email");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null; // Or throw an exception
+    }
+
     @Override
     public User getUserByUsername(String username) {
         String sql = "SELECT * FROM users WHERE username = ?";
--- src/interface_adapter/EmailSender/EmailSenderGateway.java
+++ src/interface_adapter/EmailSender/EmailSenderGateway.java
@@ -1,2 +1,5 @@
-package interface_adapter.EmailSender;public interface EmailSenderGateway {
+package interface_adapter.EmailSender;
+public interface EmailSenderGateway {
+    default void sendEmail(String recipientEmail, String emailSubject, String emailMessage) {
+    }
 }
