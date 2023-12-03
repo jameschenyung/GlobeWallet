be6ace870b9313388d2afd1f16c66322ce1e7cc8
jasminewang <haoying.wang@mail.utoronto.ca>
2023-12-03 16:36:03 -0500

    added java doc for login

--- src/use_case/login/LoginOutputBoundary.java
+++ src/use_case/login/LoginOutputBoundary.java
@@ -1,8 +1,30 @@
 package use_case.login;
 
+/**
+ * Interface defining the contract for an output boundary in the login use case.
+ * This interface should be implemented by any class (typically a presenter) that handles
+ * the presentation logic for displaying the results of a user login attempt. It defines methods
+ * for preparing the view in case of a successful login or a failure.
+ */
+
 public interface LoginOutputBoundary {
+
+    /**
+     * Prepares and presents the view for a successful login attempt.
+     * Implementing classes should handle the logic to display or communicate the success of the login process,
+     * potentially including information about the logged-in user.
+     *
+     * @param user The data containing information about the successful login, encapsulated in a LoginOutputData object.
+     */
     void prepareSuccessView(LoginOutputData user);
 
+    /**
+     * Prepares and presents the view for a failed login attempt.
+     * Implementing classes should handle the logic to display or communicate the failure of the login process,
+     * including the error message explaining the reason for failure.
+     *
+     * @param error The error message describing why the login attempt failed.
+     */
     void prepareFailView(String error);
 }
 
--- src/use_case/login/LoginOutputData.java
+++ src/use_case/login/LoginOutputData.java
@@ -1,9 +1,21 @@
 package use_case.login;
+/**
+ * Data class that encapsulates the output data for the login process.
+ * This class holds information about the outcome of a login attempt, including a success flag
+ * and a message detailing the result.
+ */
 
 public class LoginOutputData {
     private final boolean success;
     private final String message;
 
+    /**
+     * Constructs a LoginOutputData instance with specified success status and message.
+     *
+     * @param success The success flag indicating whether the login attempt was successful.
+     * @param message The message detailing the outcome of the login attempt.
+     * @throws IllegalArgumentException if the provided message is null or empty.
+     */
     public LoginOutputData(boolean success, String message) {
         if (message == null || message.trim().isEmpty()) {
             throw new IllegalArgumentException("Message cannot be null or empty.");
@@ -12,14 +24,29 @@ public class LoginOutputData {
         this.message = message;
     }
 
+    /**
+     * Gets the success status of the login attempt.
+     *
+     * @return True if the login attempt was successful, false otherwise.
+     */
     public boolean isSuccess() {
         return success;
     }
 
+    /**
+     * Gets the message detailing the outcome of the login attempt.
+     *
+     * @return The message detailing the result of the login attempt.
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Returns a string representation of the LoginOutputData.
+     *
+     * @return A string representation of the LoginOutputData.
+     */
     @Override
     public String toString() {
         return "LoginOutputData{" +
