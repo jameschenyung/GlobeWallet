e80768b25fba06ff488addfa44282aa3f4a81048
jasminewang <haoying.wang@mail.utoronto.ca>
2023-12-03 16:28:30 -0500

    added java doc for email Sender

--- src/use_case/emailSender/SendTransactionEmail_receiver.java
+++ src/use_case/emailSender/SendTransactionEmail_receiver.java
@@ -1,2 +1,8 @@
-package use_case.emailSender;public class SendTransactionEmail_receiver {
+package use_case.emailSender;
+/**
+ * Class responsible for sending transaction-related emails to the receivers of a transaction.
+ * This class might be involved in composing and dispatching emails to inform receivers
+ * about transaction details such as the sender, amount, and status of the transaction.
+ */
+public class SendTransactionEmail_receiver {
 }
--- src/use_case/emailSender/SendTransactionEmail_sender.java
+++ src/use_case/emailSender/SendTransactionEmail_sender.java
@@ -1,2 +1,9 @@
-package use_case.emailSender;public class SendTransactionEmail_sender {
+package use_case.emailSender;
+/**
+ * Class responsible for sending transaction-related emails to the senders of a transaction.
+ * This class is typically involved in composing and dispatching emails to inform senders
+ * about the details and status of their transaction, such as confirmation of the transaction,
+ * amount sent, and the receiver's information.
+ */
+public class SendTransactionEmail_sender {
 }
