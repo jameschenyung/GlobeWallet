549ebafd446c29f98eb37817c728392c718c15df
yoyogong <yoyogongtian@gmail.com>
2023-12-04 21:50:06 -0500

    Modifications - added app package - updated some Javadoc

--- src/presenter/UpdateUserPresenter.java
+++ src/presenter/UpdateUserPresenter.java
@@ -5,15 +5,37 @@ import views.UserProfilePanel;
 
 import javax.swing.*;
 
+/**
+ * The {@code UpdateUserPresenter} class serves as the presenter in the MVP (Model-View-Presenter) pattern
+ * for updating user information. It acts as an intermediary between the view ({@code UserProfilePanel}) and
+ * the use case interactor ({@code UpdateUserInteractor}), handling the communication between the view and the
+ * business logic for updating user details.
+ */
 public class UpdateUserPresenter implements UpdateUserOutputBoundary {
     private UserProfilePanel view;
     private UpdateUserInputBoundary interactor;
 
+    /**
+     * Constructs an {@code UpdateUserPresenter} with the specified user profile view and data access interface.
+     *
+     * @param view                   The {@code UserProfilePanel} that this presenter is responsible for.
+     * @param updateUserDataAccess   An implementation of {@code UpdateUserDataAccessInterface} to interact with
+     *                               user data storage.
+     */
     public UpdateUserPresenter(UserProfilePanel view, UpdateUserDataAccessInterface updateUserDataAccess) {
         this.view = view;
         this.interactor = new UpdateUserInteractor(updateUserDataAccess, this);
     }
 
+    /**
+     * Initiates the user information update process using the provided user details.
+     * If the update process encounters errors, it will instruct the view to display the appropriate message.
+     *
+     * @param email          The email address of the user, used as an identifier for update operations.
+     * @param username       The new username to be updated.
+     * @param newPassword    The new password for the user.
+     * @param repeatPassword A repeated entry of the new password for confirmation purposes.
+     */
     public void updateUserInfo(String email, String username, String newPassword, String repeatPassword) {
         try {
             UpdateUserInputData inputData = new UpdateUserInputData(email, username, newPassword, repeatPassword);
@@ -22,11 +44,22 @@ public class UpdateUserPresenter implements UpdateUserOutputBoundary {
             view.showError("An error occurred: " + e.getMessage());
         }
     }
+
+    /**
+     * Handles the case where updating user information fails. It triggers a UI update to show an error message.
+     *
+     * @param error A string describing the error that occurred during the update process.
+     */
     @Override
     public void prepareFailView(String error) {
         SwingUtilities.invokeLater(() -> view.showError(error));
     }
 
+    /**
+     * Handles the case where updating user information succeeds. It triggers a UI update to show a success message.
+     *
+     * @param user The {@code UpdateUserOutputData} containing the updated user information.
+     */
     @Override
     public void prepareSuccessView(UpdateUserOutputData user) {
         SwingUtilities.invokeLater(() -> {
--- src/use_case/updateUser/UpdateUserInteractor.java
+++ src/use_case/updateUser/UpdateUserInteractor.java
@@ -2,14 +2,34 @@ package use_case.updateUser;
 
 import use_case.signup.SignupOutputData;
 
+/**
+ * The {@code UpdateUserInteractor} class handles the business logic for updating user information.
+ * It implements the {@code UpdateUserInputBoundary} interface and uses a data access interface
+ * to interact with the data layer and an output boundary to communicate the result of the operation.
+ */
 public class UpdateUserInteractor implements UpdateUserInputBoundary{
     private final UpdateUserDataAccessInterface userDataAccess;
     private UpdateUserOutputBoundary UpdateUserOutputBoundary;
 
+    /**
+     * Constructs an {@code UpdateUserInteractor} with the specified data access and output boundary interfaces.
+     *
+     * @param userDataAccess           The data access interface to interact with user data.
+     * @param UpdateUserOutputBoundary The output boundary interface to communicate the results of the update operation.
+     */
     public UpdateUserInteractor(UpdateUserDataAccessInterface userDataAccess, UpdateUserOutputBoundary UpdateUserOutputBoundary) {
         this.userDataAccess = userDataAccess;
         this.UpdateUserOutputBoundary = UpdateUserOutputBoundary;
     }
+
+    /**
+     * Executes the update operation with the provided user input data.
+     * This method validates the provided information, updates the user data if valid,
+     * and communicates the outcome through the output boundary.
+     *
+     * @param updateUserInputData The input data for the update operation, containing new user information.
+     * @throws Exception If an error occurs during the update process.
+     */
     @Override
     public void execute(UpdateUserInputData updateUserInputData) throws Exception {
         if (!(updateUserInputData.getRepeatNewPassword().equals(updateUserInputData.getNewPassword()))){
